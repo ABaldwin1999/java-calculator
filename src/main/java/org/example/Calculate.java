31ac1e08e6927b174acb31e98296416b8d4fc27a
Amelia Baldwin <amelia01000010@gmail.com>
2022-12-06 20:20:50 +0000

    preparing equation for calculation

--- src/main/java/org/example/Calculate.java
+++ src/main/java/org/example/Calculate.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Calculate {
     public double calculate(String input) {
-        double answer=0;
+        double answer;
         String newInput = prepareFrontOfEquation(input);
         String equation = getEquation(removeDuplicateOrSuperfluousOperators(newInput),"");
         if(equationLength(equation) == 3){
@@ -32,108 +32,89 @@ public class Calculate {
     }
 
     private String getEquation(String input,String operator) {
-        String equation = input;
+        StringBuilder equation = new StringBuilder(input);
         String[] inputArr = makeDecimalArray(input);
-        if (equation.isBlank()){
-            equation="0";
+        if (equation.toString().isBlank()){
+            equation = new StringBuilder("0");
         }
         else if(operator.equals("+")||operator.equals("-")||operator.equals("/")||operator.equals("*")||operator.equals("^")){
             for (int i = 0; i < inputArr.length ; i++) {
                 if(inputArr[i].equals(operator)){
-                    equation = inputArr[i-1]+inputArr[i]+inputArr[i+1];
+                    equation = new StringBuilder(inputArr[i - 1] + inputArr[i] + inputArr[i + 1]);
                 }
             }
         } else if (operator.equals("(")) {
-            equation="";
+            equation = new StringBuilder();
             int openingBrackets =0;
             int closingBrackets =0;
-            for (int i = 0; i < inputArr.length ; i++) {
-                if(inputArr[i].equals(operator) && openingBrackets!=closingBrackets){
+            for (String s : inputArr) {
+                if (s.equals(operator) && openingBrackets != closingBrackets) {
                     openingBrackets++;
-                    equation += inputArr[i];
+                    equation.append(s);
                 }
-                if(inputArr[i].equals(")") && openingBrackets!=closingBrackets){
+                if (s.equals(")") && openingBrackets != closingBrackets) {
                     closingBrackets++;
-                    equation += inputArr[i];
+                    equation.append(s);
                 }
             }
-            ///check there are no more brackets inside
-            //check number even and odd brackets the same
+
         }
-        return equation;
+        return equation.toString();
     }
     public String[] makeDecimalArray(String input){
-        String[] inputArr;
+        String[] inputArr = new String[equationLength(input)];
         int i =0;
         boolean operator = false;
         for (int j = 0; j < input.length(); j++) {
             if(operator){
                 i++;
-                inputArr[i]=inputArr[i];
                 if(!inputArr[i-1].contains(".")){
-                inputArr[i-1] = inputArr[i-1]+".";
+                inputArr[i-1] = inputArr[i-1]+". ";
              }
                 else {
                 inputArr[i]=inputArr[i]+input.split("")[j];
                 }
             }
-            operator =currentCharOperator();///input current character
+            operator =currentCharOperator(input.split("")[j]);///input current character
         }
         return inputArr;
     }
+
+    private boolean currentCharOperator(String s) {
+        return true;
+    }
+
     public int equationLength(String equation){
         ///count number of operators then *2+1
         return 3;
     }
     public double BODMAS(String equation){
-        double answer=0;
         String calculation = equation;
-        ///most of this can be boiled down to one repeating function
-        equation.indexOf("(");
         if(equation.contains("(")){
-
-        //calculation = calculation.replace(,);
+            calculation = calculation.replace(getEquation(calculation,"^"),""+calculate(getEquation(calculation,"(")));
     } else if (equation.contains("^")) {
-            calculation.substring(FindNumberIndexToTheLeftOfTheOperator("^"), FindNumberIndexToTheRightOfTheOperator("^"));
-
-
-            //calculation = calculation.replace(,);
+            calculation = calculation.replace(getEquation(calculation,"^"),""+calculate(getEquation(calculation,"^")));
     } else if (equation.contains("/")) {
-            //calculation = calculation.replace(,);
+            calculation = calculation.replace(getEquation(calculation,"^"),""+calculate(getEquation(calculation,"/")));
         }
     else if (equation.contains("*")) {
-            //calculation = calculation.replace(,);
+            calculation = calculation.replace(getEquation(calculation,"^"),""+calculate(getEquation(calculation,"*")));
         }
     else if (equation.contains("+")) {
-            //calculation = calculation.replace(,);
+            calculation = calculation.replace(getEquation(calculation,"^"),""+calculate(getEquation(calculation,"+")));
         }
     else if (equation.contains("-")) {
-            //calculation = calculation.replace(,);
+            calculation = calculation.replace(getEquation(calculation,"^"),""+calculate(getEquation(calculation,"-")));
         }
-        return 0;
+        return Double.parseDouble(calculation);
     }
 
-    private int FindNumberIndexToTheLeftOfTheOperator(String calculation) {
-        for (int i = calculation.length(); i > 0; i--) {
-            if(calculation.split("")[i].equals("^")){
-                return i+1;
-            }
-        }
-        return 0;
-    }
-    private int FindNumberIndexToTheRightOfTheOperator(String calculation) {
-        for (int i = 0; i <calculation.length(); i++) {
-            if(calculation.split("")[i].equals("^")){
-                return i;
-            }
-        }
-        return calculation.length()+1;
-    }
+
     public double operations(String input){
-        double answer =0.;
-        double num0=1.;
-        double num1=1.;
-        String operator = "";
+        double answer=0;
+        double num0= Double.parseDouble((input.split(" ")[0]));
+        double num1=Double.parseDouble((input.split(" ")[2]));
+        String operator = input.split(" ")[1];
         switch(operator) {
             case "+":
                 answer = num0 + num1;
